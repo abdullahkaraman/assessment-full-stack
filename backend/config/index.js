6aaf0a2538634ca2c3110397b9064b5172e861d0
abdullahkaraman <abdullahkaramaniletisim@gmail.com>
2024-02-11 15:11:21 +0300

    Update MongoDB URI configuration

--- backend/config/index.js
+++ backend/config/index.js
@@ -1,1 +1,4 @@
-export const MONGODB_URI = "mongodb://localhost:27017/tfashion";
+import dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
+export const MONGODB_URI = process.env.MONGODB_URI;
